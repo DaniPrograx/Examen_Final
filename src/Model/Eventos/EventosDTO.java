194f2122f3ea64ea53f262bd37587267f50f2f7b
Daniel Hidalgo <dm080032@gmail.com>
2024-11-26 09:25:44 -0600

    Se realiza la clase EventosDTO

--- src/Model/Eventos/EventosDTO.java
+++ src/Model/Eventos/EventosDTO.java
@@ -12,9 +12,36 @@ import DatabaseConnection.ConnectionBD;
  */
 public class EventosDTO {
     private ConnectionBD connection;
-
+private int id;
+    private String nombre ;
+    private String lugar;
+    private String capacidad;
     public EventosDTO(ConnectionBD connection) {
         this.connection = connection;
     }
+
+    public ConnectionBD getConnection() {
+        return connection;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+   
+    
+    
     
 }
