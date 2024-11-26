0ccbbf236cf7fafc9bade859f0a86687c205e42b
Isaac Sibaja <isaacsibaja901@gmail.com>
2024-11-26 09:41:55 -0600

    se crea la clase eventosmapper

---
+++ src/Model/Eventos/EventosMapper.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Eventos;
+
+import Model.Mapper.Mapper;
+
+/**
+ *
+ * @author Student
+ */
+public class EventosMapper implements Mapper<Eventos,EventosDTO>{
+
+    @Override
+    public EventosDTO toDTo(Eventos ent) {
+    
+        return null;
+    
+    }
+
+    @Override
+    public Eventos toEnt(EventosDTO dto) {
+      
+        return null;
+      
+    }
+    
+}
