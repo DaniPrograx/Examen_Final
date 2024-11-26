17b617cf7f883af3957a62e6dddb2af124fb2256
Isaac Sibaja <isaacsibaja901@gmail.com>
2024-11-26 09:03:09 -0600

    se crea paquete con su clase modelo

---
+++ src/Model/Eventos/Eventos.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Eventos;
+
+/**
+ *
+ * @author Student
+ */
+public class Eventos {
+    private int id;
+    private String nombre ;
+    private String lugar;
+    private String capacidad;
+
+    public Eventos(int id, String nombre, String lugar, String capacidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.lugar = lugar;
+        this.capacidad = capacidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+    
+    
+    
+}
